70f68c5ae7ff81a6668f5f5a7ba2668fb56765e8
Daniel Yuan <colinss.daniell530@gmail.com>
2023-04-05 00:20:31 +0800

    commit 1

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
@@ -1,15 +1,33 @@
 package cn.edu.sustech.cs209.chatting.client;
 
 import cn.edu.sustech.cs209.chatting.common.Message;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.util.Callback;
 
 import java.net.URL;
@@ -19,34 +37,134 @@ import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Controller implements Initializable {
-
+    Stage stage;
+    @FXML
+    VBox root;
     @FXML
     ListView<Message> chatContentList;
+    @FXML
+    ListView<String> chatList;
+    @FXML
+    TextArea inputArea;
+
+    HashMap<String, ArrayList<Message>> usersDialogSet = new HashMap<>();
 
     String username;
 
+    String currentChatMate;
+    Socket client;
+    Scanner in;
+    PrintWriter out;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        chatList.getSelectionModel().selectedItemProperty().addListener(
+            (observable, oldValue, newValue) -> {
+                if(newValue!=oldValue){
+                    usersDialogSet.get(currentChatMate).clear();
+                    usersDialogSet.get(currentChatMate).addAll(chatContentList.getItems());
+
+                    chatContentList.getItems().clear();
+                    chatContentList.getItems().setAll(usersDialogSet.get(newValue));
+                    currentChatMate = newValue;
+                }
+            }
+        );
 
         Dialog<String> dialog = new TextInputDialog();
         dialog.setTitle("Login");
         dialog.setHeaderText(null);
         dialog.setContentText("Username:");
 
+        registerSocket();
+
+
         Optional<String> input = dialog.showAndWait();
         if (input.isPresent() && !input.get().isEmpty()) {
+
             /*
                TODO: Check if there is a user with the same name among the currently logged-in users,
                      if so, ask the user to change the username
              */
             username = input.get();
+
+            while(!registerServer()){
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Duplicated name");
+                alert.setContentText("User name is already exist. Change your name.");
+                alert.showAndWait();
+                input = dialog.showAndWait();
+                username = input.get();
+            }
         } else {
             System.out.println("Invalid username " + input + ", exiting");
+            quitServer();
             Platform.exit();
         }
 
         chatContentList.setCellFactory(new MessageCellFactory());
+        chatContentList.setItems(FXCollections.observableArrayList());
+
+        stage = (Stage)root.getScene().getWindow();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                if(windowEvent.getSource() == stage){
+                    quitServer();
+                }
+            }
+        });
     }
+
+
+    public void registerSocket(){
+        try {
+            client = new Socket("localhost",135);
+            in = new Scanner(client.getInputStream());
+            out = new PrintWriter(client.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void quitServer(){
+        out.println("QUIT");
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+    public boolean registerServer(){
+        out.println("REGISTER  "+username);
+        out.flush();
+        String response = in.next();
+        if(response.equals("OK")){
+            return true;
+        }else if(response.equals("DENY")){
+            return false;
+        }
+        return true;
+    }
+
+    public ArrayList<String> getServerUsers(){
+        out.println("GET");
+        out.flush();
+
+        ArrayList<String> output = new ArrayList<>(5);
+        String nextLine = null;
+        while((nextLine = in.next())!=null){
+            if(nextLine != username){
+                output.add(nextLine);
+            }
+            if(nextLine.equals(in.next())){
+                break;
+            }
+        }
+        return output;
+    }
+
+
 
     @FXML
     public void createPrivateChat() {
@@ -56,12 +174,34 @@ public class Controller implements Initializable {
         ComboBox<String> userSel = new ComboBox<>();
 
         // FIXME: get the user list from server, the current user's name should be filtered out
-        userSel.getItems().addAll("Item 1", "Item 2", "Item 3");
+        ArrayList<String>users = getServerUsers();
+
+        userSel.getItems().addAll(users);
 
         Button okBtn = new Button("OK");
         okBtn.setOnAction(e -> {
             user.set(userSel.getSelectionModel().getSelectedItem());
             stage.close();
+
+
+
+            //Add new a user chatting item to the left pane
+            if(usersDialogSet.size()!=0){
+                usersDialogSet.get(currentChatMate).clear();
+                usersDialogSet.get(currentChatMate).addAll(chatContentList.getItems());
+                chatContentList.getItems().clear();
+            }
+
+
+            if(chatList.getItems().contains(user.get())){
+                //open the chat with user selected
+                chatContentList.getItems().setAll(usersDialogSet.get(user.get()));
+            }else{
+                chatList.getItems().add(user.get());
+                usersDialogSet.put(user.get(),new ArrayList<>(5));
+                chatContentList.getItems().setAll(usersDialogSet.get(user.get()));
+            }
+            currentChatMate = user.get();
         });
 
         HBox box = new HBox(10);
@@ -70,6 +210,7 @@ public class Controller implements Initializable {
         box.getChildren().addAll(userSel, okBtn);
         stage.setScene(new Scene(box));
         stage.showAndWait();
+
 
         // TODO: if the current user already chatted with the selected user, just open the chat with that user
         // TODO: otherwise, create a new chat item in the left panel, the title should be the selected user's name
@@ -97,7 +238,14 @@ public class Controller implements Initializable {
      */
     @FXML
     public void doSendMessage() {
-        // TODO
+        String text = inputArea.getText();
+        //send to
+        Message message = new Message(System.currentTimeMillis(),username,"Yuan",text);
+        chatContentList.getItems().add(message);
+        inputArea.clear();
+
+
+        // TODO: Trans to server
     }
 
     /**
@@ -113,6 +261,9 @@ public class Controller implements Initializable {
                 public void updateItem(Message msg, boolean empty) {
                     super.updateItem(msg, empty);
                     if (empty || Objects.isNull(msg)) {
+                        setGraphic(null);
+                        setText(null);
+
                         return;
                     }
 
--- chatting-server/src/main/java/cn/edu/sustech/cs209/chatting/server/Main.java
+++ chatting-server/src/main/java/cn/edu/sustech/cs209/chatting/server/Main.java
@@ -1,8 +1,114 @@
 package cn.edu.sustech.cs209.chatting.server;
 
-public class Main {
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main {
+    static UserSet userSet;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Starting server");
+        ServerSocket server = new ServerSocket(135);
+        userSet = new UserSet();
+
+        while(true){
+            Socket socket = server.accept();
+            handle han = new handle(userSet,socket);
+            Thread thread = new Thread(han);
+            thread.start();
+        }
     }
+}
+
+class handle implements Runnable{
+    UserSet userSet;
+    Socket clientSocket;
+    Scanner in;
+    PrintWriter out;
+    String userName;
+    public handle(UserSet userset,Socket clientsocket) throws IOException {
+        userSet = userset;
+        clientSocket = clientsocket;
+        in = new Scanner(clientSocket.getInputStream());
+        out = new PrintWriter(clientSocket.getOutputStream());
+    }
+
+    public void execute(String command){
+        switch(command){
+            case "REGISTER":
+                userName = in.next();
+                if(userSet.userNameSet.contains(userName)){
+                    out.println("DENY");
+                    out.flush();
+                }else{
+                    userSet.addUser(userName,clientSocket);
+                    userSet.userNameSet.add(userName);
+                    out.println("OK");
+                    out.flush();
+                }
+                break;
+            case "CONNECT":
+
+                break;
+            case "GET":
+                System.out.println(1);
+                ArrayList<String> userList = userSet.getUserNameSet();
+                for (int i = 0; i < userList.size(); i++) {
+                    out.println(userList.get(i));
+                    out.flush();
+                }
+                out.println();
+                out.flush();
+                System.out.println(2);
+                break;
+        }
+    }
+    @Override
+    public void run() {
+        try{
+            while(true){
+                String command = in.next();
+                if(command.equals("QUIT")){
+                    userSet.userQuit(userName);
+                    return;
+                }
+                execute(command);
+            }
+        } finally {
+            in.close();
+            out.close();
+            userSet.userQuit(userName);
+        }
+    }
+}
+class UserSet{
+    ArrayList<String>userNameSet = new ArrayList<>(5);
+    HashMap<String,Socket>clientServer = new HashMap<>();
+    public UserSet() {}
+    public synchronized void addUser(String userName, Socket client){
+        userNameSet.add(userName);
+        clientServer.put(userName,client);
+    }
+
+    public synchronized Socket getClient(String userName){
+        return clientServer.get(userName);
+    }
+
+    public synchronized void userQuit(String userName){
+        userNameSet.remove(userName);
+        clientServer.remove(userName);
+    }
+
+    public synchronized ArrayList<String> getUserNameSet(){
+        return userNameSet;
+    }
+
+
 }
